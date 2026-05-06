d06a9a35b18404b12fbbfe2ed70e85d7ff6f8fd7
namle24 <nma2409.na@gmail.com>
2026-05-06 20:32:54 +0700

    Labwork 1: Gradient Descent

---
+++ Lab1/gradientdescent.py
@@ -0,0 +1,18 @@
+def f(x):
+    return x*x
+
+def df(x):
+    return 2*x
+
+def GradientDescent (x, lr, thread_hold):
+	count = 0
+	while abs(f(x)> thread_hold):
+		x = x - lr * df(x)
+		count += 1
+		print(f"{count} {x} {df(x)}")
+
+if __name__ == "__main__":
+	x = 10
+	lr = 0.9
+	thread_hold = 0.01
+	GradientDescent(x, lr, thread_hold)
